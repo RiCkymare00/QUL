15794b626bdc1065469c827471c00f9310ba6405
RiCkymare00 <maregariccardo.rm0@gmail.com>
2024-02-08 12:26:41 +0100

    hydrogen atom

--- particle_system.py
+++ particle_system.py
@@ -2,7 +2,9 @@ from constants import *
 import numpy as np 
 import matplotlib.pyplot as plt
 import scipy.special
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def V (x_values):
      Energy = np.zeros_like(x_values)
@@ -24,7 +26,7 @@ def norm(psi,x_values):
     return normal
 
 def radial_wave_function(r, n, l):
-    """Calcola la funzione d'onda radiale dell'atomo di idrogeno per un valore specifico di r."""
+
     rho = 2 * r / n
     radial_factor = np.sqrt((2 / (n**3 * np.math.factorial(n))) * np.exp(-rho) * rho**l)
     laguerre = np.polyval(np.polyder(np.poly1d([(-1)**l, 2*l+1, -n+l, n**2 * (n+1)])), rho)
@@ -32,7 +34,7 @@ def radial_wave_function(r, n, l):
     return R
 
 def angular_wave_function(theta, phi, l, m):
-    """Calcola la funzione d'onda angolare dell'atomo di idrogeno per valori specifici di theta e phi."""
+
     Y = scipy.special.sph_harm(m, l, phi, theta)
     return Y
 
@@ -56,31 +58,35 @@ class wave_function:
             plt.show()
           
         elif problema == 'atomo idrogeno':
-            for n in range (1,2):
-                l = n-1
-                for m in range(-l, l+1):
-                    psi = []
-                    X_values = []
-                    y_values = []
-                    z_values = []
-                    theta_values = np.linspace(0, np.pi/2, 10)
-                    phi_values = np.linspace(0, np.pi, 10)
-                    for r in x_values:
-                        for theta in theta_values:
-                            for phi in phi_values:
-                                R = radial_wave_function(r, n, l)
-                                Y = angular_wave_function(theta, phi, l, m)
-                                psi.append(np.abs(R * Y)**2)
-                                x = r * np.sin(theta) * np.cos(phi)
-                                y = r * np.sin(theta) * np.sin(phi)
-                                z = r * np.cos(theta)
-                                X_values.append(x)
-                                y_values.append(y)
-                                z_values.append(z)         
-            fig = go.Figure(data=go.Scatter3d(x=X_values, y=y_values, z=z_values, mode='markers', marker=dict(size=5, color=psi, colorscale='Viridis', opacity=0.8)))
-            fig.update_layout(scene=dict(xaxis=dict(title='x'), yaxis=dict(title='y'), zaxis=dict(title='z')), title='Funzione d\'onda $\psi$ in coordinate cartesiane', autosize=True)
-            fig.show()
-            fig.write_image("funzione_onda_atomo_idrogeno.png")
+            n = 4
+            l = 3
+            m = 1
+            psi = []
+            X_values = []
+            y_values = []
+            z_values = []
+            theta_values = np.linspace(0, np.pi, 100)
+            phi_values = np.linspace(0, 2*np.pi, 100)
+            for r in x_values:
+                for theta in theta_values:
+                    for phi in phi_values:
+                        R = radial_wave_function(r, n, l)
+                        Y = angular_wave_function(theta, phi, l, m)
+                        psi.append(np.abs(R * Y)**2)
+                        x = np.abs(R * Y)**2 * np.sin(theta) * np.cos(phi)
+                        y = np.abs(R * Y)**2 * np.sin(theta) * np.sin(phi)
+                        z = np.abs(R * Y)**2 * np.cos(theta)
+                        X_values.append(x)
+                        y_values.append(y)
+                        z_values.append(z)         
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(X_values, y_values, z_values, c=psi, cmap='viridis', alpha=0.5)
+            ax.set_title('$\psi$ atomo idrogeno; n: {}, l: {}, m: {}'.format(n, l, m))
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            plt.show()
     
         ''' Si richiede all'utente di definire il numero di dimensioni ed il tipo di sistema che si vuole studiare
         '''
